dbb360cd0ae1d22a340758720f2eed6b952fbb17
Serge Balandine <serge.balandine@epicentre.msf.org>
2025-05-23 17:18:27 +0200

    .

--- cron_d2.R
+++ cron_d2.R
@@ -1,7 +1,7 @@
 library(utils)
 library(stats)
 library(lubridate)
-library("git2r")
+library('git2r')
 
 setwd('C:/Users/Administrateur/Desktop/typology')
 
@@ -9,65 +9,67 @@ RODBC::odbcCloseAll()
 
 # 10.129.22.187 OLD SERVER=10.129.16.51;
 
-rodbc<-NULL
-rodbc<-RODBC::odbcDriverConnect(
-  "DRIVER={ODBC Driver 13 for SQL Server};
+rodbc <- NULL
+rodbc <- RODBC::odbcDriverConnect(
+  'DRIVER={ODBC Driver 13 for SQL Server};
   SERVER=10.129.22.187;
   Database=d2epicentre;
   uid=FWA_ro;
-  pwd=LWs5gt#9jU7d")
+  pwd=LWs5gt#9jU7d')
 
 
-catf<-function(...){
+catf <- function(...) {
   cat(sprintf(...))
   flush.console()
 }
 
-normalizeText<-function(x){
+normalizeText <- function(x) {
   # x <- iconv(x, to='ASCII//TRANSLIT')
   x <- gsub('[[:cntrl:]]+', ' ', x)
   x <- gsub('\n', ' ', x)
   x <- gsub(',', ' ', x)
-  return(x)
+  x
 }
 
-normalizeDate<-function(x){
+normalizeDate <- function(x) {
   x <- as.character(x)
   x <- gsub('[0-9]{2}:[0-9]{2}:[0-9]{2}', '', x)
-  return(x)
+  x
 }
 
-dumpTable<-function(tablename){
-  catf('Process table "%s"',tablename)
+dumpTable <- function(tablename) {
+  catf('Process table "%s"', tablename)
   rodbc <- RODBC::odbcReConnect(rodbc)
-  df<-RODBC::sqlQuery(rodbc, sprintf("SELECT * FROM %s", tablename), stringsAsFactors = FALSE)
-  df<-dplyr::mutate_if(df, is.character, normalizeText)
-  df<-dplyr::mutate_if(df, is.POSIXct, normalizeDate)
-  if(nrow(df)>0) {
-    df<-dplyr::mutate(df, rownames = 1:dplyr::n())
+  df <- RODBC::sqlQuery(rodbc, sprintf('SELECT * FROM %s', tablename), stringsAsFactors = FALSE)
+  df <- dplyr::mutate_if(df, is.character, normalizeText)
+  df <- dplyr::mutate_if(df, is.POSIXct, normalizeDate)
+  if (nrow(df) > 0L) {
+    df <- dplyr::mutate(df, rownames = seq_len(dplyr::n()))
   } else {
-    df<-dplyr::mutate(df, rownames = as.integer())
+    df <- dplyr::mutate(df, rownames = as.integer())
   }
-  df<-dplyr::select(df, rownames, dplyr::everything())
-  readr::write_csv(df,path=sprintf('C:/Users/Administrateur/Desktop/typology/data/%s.csv',tolower(tablename)), na = '')
-  
+  df <- dplyr::select(df, rownames, dplyr::everything())
+  readr::write_csv(df, path = sprintf('C:/Users/Administrateur/Desktop/typology/data/%s.csv', tolower(tablename)), na = '')
+
 }
 
 tables <- RODBC::sqlTables(rodbc)
-tables <- subset(tables, TABLE_TYPE == "TABLE")
-tables <- subset(tables, TABLE_NAME != "ArcoleItemsImporting")
+tables <- subset(tables, TABLE_TYPE == 'TABLE')
+tables <- subset(tables, TABLE_NAME != 'ArcoleItemsImporting')
 
-for(i in 1:nrow(tables)) {
-  tryCatch({
-    dumpTable(tables[i,'TABLE_NAME'])
-    catf('...Ok\n')
-  }, error = function(e) {
-    catf('...Ko\n')
-    message(e)
-  })
+for (i in 1:nrow(tables)) {
+  tryCatch(
+    {
+      dumpTable(tables[i, 'TABLE_NAME'])
+      catf('...Ok\n')
+    },
+    error = function(e) {
+      catf('...Ko\n')
+      message(e)
+    })
 }
 
-Sys.setenv(GITHUB_PAT="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")
+Sys.setenv(GITHUB_PAT = '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')
 
 system2('git', 'commit -ma "."')
 system2('git', 'push')
--- cron_d2formation.R
+++ cron_d2formation.R
@@ -4,51 +4,51 @@ library(lubridate)
 
 setwd('C:/Users/Administrateur/Desktop/typology')
 
-rodbc<-RODBC::odbcDriverConnect(
-  "DRIVER={ODBC Driver 13 for SQL Server};
+rodbc <- RODBC::odbcDriverConnect(
+  'DRIVER={ODBC Driver 13 for SQL Server};
   SERVER=10.129.22.187;
   Database=d2epicentre_formation;
   uid=FWA_ro;
-  pwd=LWs5gt#9jU7d")
+  pwd=LWs5gt#9jU7d')
 
 
-catf<-function(...){
+catf <- function(...) {
   cat(sprintf(...))
   flush.console()
 }
 
-normalizeText<-function(x){
-  x <- iconv(x, to='ASCII//TRANSLIT')
+normalizeText <- function(x) {
+  x <- iconv(x, to = 'ASCII//TRANSLIT')
   x <- gsub('[[:cntrl:]]+', ' ', x)
   x <- gsub('\n', ' ', x)
   x <- gsub(',', ' ', x)
-  return(x)
+  x
 }
 
-normalizeDate<-function(x){
+normalizeDate <- function(x) {
   x <- as.character(x)
   x <- gsub('[0-9]{2}:[0-9]{2}:[0-9]{2}', '', x)
-  return(x)
+  x
 }
 
-dumpTable<-function(tablename){
-  catf('Process table "%s"',tablename)
-  df<-RODBC::sqlQuery(rodbc, sprintf("SELECT * FROM %s", tablename), stringsAsFactors = FALSE)
-  df<-dplyr::mutate_if(df, is.character, normalizeText)
-  df<-dplyr::mutate_if(df, is.POSIXct, normalizeDate)
-  if(nrow(df)>0) {
-    df<-dplyr::mutate(df, rownames = 1:dplyr::n())
+dumpTable <- function(tablename) {
+  catf('Process table "%s"', tablename)
+  df <- RODBC::sqlQuery(rodbc, sprintf('SELECT * FROM %s', tablename), stringsAsFactors = FALSE)
+  df <- dplyr::mutate_if(df, is.character, normalizeText)
+  df <- dplyr::mutate_if(df, is.POSIXct, normalizeDate)
+  if (nrow(df) > 0L) {
+    df <- dplyr::mutate(df, rownames = seq_len(dplyr::n()))
   } else {
-    df<-dplyr::mutate(df, rownames = as.integer())
+    df <- dplyr::mutate(df, rownames = as.integer())
   }
-  df<-dplyr::select(df, rownames, dplyr::everything())
-  readr::write_csv(df,path=sprintf('C:/Users/Administrateur/MSF/GRP-EPI-PROJ-TypoEpicentre - Dump/Epicentre (Formation)/%s.csv',tolower(tablename)), na = '')
+  df <- dplyr::select(df, rownames, dplyr::everything())
+  readr::write_csv(df, path = sprintf('C:/Users/Administrateur/MSF/GRP-EPI-PROJ-TypoEpicentre - Dump/Epicentre (Formation)/%s.csv', tolower(tablename)), na = '')
   catf('...Ok\n')
 }
 
 tables <- RODBC::sqlTables(rodbc)
-tables <- subset(tables, TABLE_TYPE == "TABLE")
-tables <- subset(tables, TABLE_TYPE != "ArcoleItemsImporting")
+tables <- subset(tables, TABLE_TYPE == 'TABLE')
+tables <- subset(tables, TABLE_TYPE != 'ArcoleItemsImporting')
 
-for(i in 1:nrow(tables))
-  dumpTable(tables[i,'TABLE_NAME'])
+for (i in 1:nrow(tables))
+  dumpTable(tables[i, 'TABLE_NAME'])
